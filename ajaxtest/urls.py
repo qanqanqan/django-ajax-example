f7df80ab029e32506b8a676681ac48817ad779c0
qanqanqan <fortissimusoffi@gmail.com>
2024-06-20 12:21:35 +0400

    Added POST method example with AJAX. Structured GET and POST methods to different urls

--- ajaxtest/urls.py
+++ ajaxtest/urls.py
@@ -5,6 +5,11 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+
+    # Getting data with GET method
+    path('profiles/', show_profiles, name='profiles'),
     path('get-profiles/', get_profiles, name='get_profiles'),
 
+    # Creating data with POST method
+    path('change-form/', change_form, name='change_form')
 ]
--- ajaxtest/views.py
+++ ajaxtest/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from .models import Profile
 
@@ -8,6 +8,29 @@ def index(req):
     return render(req, 'ajaxtest/index.html')
 
 
+# ==================================================
+# Getting data with GET method
+def show_profiles(req):
+    return render(req, 'ajaxtest/profiles.html')
+
+
 def get_profiles(req):
     profiles = Profile.objects.all()
     return JsonResponse({'profiles': list(profiles.values())})
+# ==================================================
+
+
+# ===================================
+# Creating data with POST method
+def change_form(req):
+    if req.method == 'POST':
+        name = req.POST['name']
+        email = req.POST['email']
+        is_staff = True if req.POST['is_staff'] == 'on' else False
+
+        new_profile = Profile(name=name, enail=email, is_staff=is_staff)
+        new_profile.save()
+
+        return HttpResponse('Profile has been created')
+    return render(req, 'ajaxtest/form.html')
+# ===================================
